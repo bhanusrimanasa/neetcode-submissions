e5804826e230ec6fab0bad8d3a31b69c3db2b59e
M.Bhanu Sri Manasa <bhanusrimanasa@gmail.com>
2026-08-06 00:14:32 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       int n=s.length();
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                st.push(s[i]);
+            }
+            if(s[i]==')'){
+                if(!st.empty()&&st.top()=='('){st.pop();}
+                else{
+                    return false;
+                }
+            }
+            if(s[i]=='}'){
+                if(!st.empty()&&st.top()=='{'){st.pop();}
+                else{
+                    return false;
+                }
+            }
+            if(s[i]==']'){
+                if(!st.empty()&&st.top()=='['){st.pop();}
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty())return true;
+        return false;
+        
+    }
+};
