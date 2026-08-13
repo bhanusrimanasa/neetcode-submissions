4059edcdd06f4cc67d5b5b44435518998ef6fbcc
M.Bhanu Sri Manasa <bhanusrimanasa@gmail.com>
2026-08-13 17:15:52 +0530

    Add: decode-ways - submission-4

---
+++ Data Structures & Algorithms/decode-ways/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n=s.length();
+        vector<int>dp(n+1,0);
+       
+        if(n==0||s[0]=='0')return 0;
+        if(n==1)return 1;
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+             if(s[i-1]!='0'){
+            dp[i]+=dp[i-1];
+        }
+        int two=(s[i-2]-'0')*10+(s[i-1]-'0');
+        if(two>=10&&two<=26){
+            dp[i]+=dp[i-2];
+        }
+        }
+        return dp[n];
+       
+    }
+};
